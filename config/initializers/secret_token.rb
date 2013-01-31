9bc2877d34dd2f022277332919c06e04161d0ddb
Kenichi Kanai <kenichi.kanai@gmail.com>
2013-01-31 14:54:03 +0900

    initialize rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GuardTest::Application.config.secret_token = '24fb2aabdcce538e892d1c65ba68e295e3fce88d65c9f912feadfa4087ce4f45019dfda405e53c21616b5c473188d73c49bca515a78edc054e8150471c7a6405'
